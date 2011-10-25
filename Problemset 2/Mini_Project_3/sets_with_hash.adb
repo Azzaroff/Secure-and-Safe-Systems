a5bba5dba82201cd1dbd8ff08f223803e2d7f740
Azzaroff <azzaroff@felux.fritz.box>
2011-10-25 21:54:30 +0200

    alles implementiert

--- Problemset 2/Mini_Project_3/sets_with_hash.adb
+++ Problemset 2/Mini_Project_3/sets_with_hash.adb
@@ -4,7 +4,7 @@ package body sets_with_hash is
 function To_Set ( Item : Item_Type ) return Set is
 A : Set;
 begin
-A.Insert(Item);
+ Insert(A, Item);
 return A;
 end To_Set;
 
@@ -17,7 +17,7 @@ end Empty_Set;
 procedure Insert (A: in out Set ; Item : Item_Type ) is
       dummy : Dummy_Type;
 begin
-
+      --ignore items of the map, use hash as items for the set!
       Maps.Insert(Maps.map(A),
                      Key => item,
                   New_Item => dummy);
@@ -25,43 +25,91 @@ begin
 end Insert;
 
 function "and" (A, B : Set ) return Set is
+     C : Set;
+     elemA: Maps.Cursor := First(A);
+     elemB: Maps.Cursor := First(B);
 begin
-   return A; --Dummy
+    while Maps.Has_Element(elemA) loop
+       C.Insert( Maps.Key(elemA));
+       Maps.Next(elemA);
+    end loop;
+
+    while Maps.Has_Element(elemB) loop
+       C.Insert( Maps.Key(elemB));
+       Maps.Next(elemB);
+    end loop;
+    return C;
 end "and"; -- union
 
 function "or" (A, B : Set ) return Set is
+      C : Set;
+      elemA: Maps.Cursor := First(A);
+      elemB: Maps.Cursor := First(B);
 begin
-   return A; --Dummy
+      while Maps.Has_Element(elemA) loop
+         while Maps.Has_Element(elemB) loop
+            if (Maps.Key(elemA) = Maps.Key(elemB)) then
+               C.Insert( Maps.Key(elemA));
+            end if;
+            Maps.Next(elemB);
+         end loop;
+         Maps.Next(elemA);
+      end loop;
+      return C;
 end "or"; -- intersection
 
 function "-" (A, B : Set ) return Set is
+      C : Set := A;
+      elemA: Maps.Cursor := First(A);
+      elemB: Maps.Cursor := First(B);
 begin
-   return A; --Dummy
+      while Maps.Has_Element(elemA) loop
+         while Maps.Has_Element(elemB) loop
+            if (Maps.Key(elemA) = Maps.Key(elemB)) then
+               Delete(C, Maps.Key(elemB));
+            end if;
+            Maps.Next(elemB);
+         end loop;
+         Maps.Next(elemA);
+      end loop;
+      return C;
 end "-"; -- difference
 
 function "<=" (A, B : Set ) return Boolean is
+      elemA: Maps.Cursor := First(A);
 begin
-   return true; --Dummy
+      while Maps.Has_Element(elemA) loop
+         if (To_Set(Maps.Key(elemA)) <= B) then
+            return true;
+         end if;
+         Maps.Next(elemA);
+      end loop;
+      return false;
 end "<="; -- A subset of B
 
 function "<" (A, B : Set ) return Boolean is
 begin
-   return true; --Dummy
+   if (Natural((Maps.Length(Maps.map(A)))) /= Natural(Maps.Length(Maps.map(B)))) then
+      return "<="(A,B);
+   else
+      return false;
+   end if;
 end "<"; -- A propper subset of B
 
 function "=" (A, B : Set ) return Boolean is
 begin
-   return true; --Dummy
+   return (Maps."="(Maps.map(A) ,Maps.map(B))); --Dummy
 end "="; -- A and B are equal
 
 procedure Delete (A: in out Set ; Item : Item_Type ) is
 begin
-   null;
+   Maps.Delete(Maps.map(A),Item);
 end Delete; -- A − { Item }
 
 function "<=" ( Item : Item_Type ; S : Set ) return Boolean is
-begin
-   return true;
+   begin
+   -- Maps.Is_In findet er nicht obwohl es in der Referenz steht, deswegen dieser "Hack"
+   return Maps.Has_Element(Maps.find(Maps.map(S), item));
 end "<="; -- { Item } <= S
 
 
