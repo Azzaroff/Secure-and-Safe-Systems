4f2b0701e2cec3492ca8dd4bb9467377ededfa82
Azzaroff <azzaroff@felux.fritz.box>
2011-10-30 14:20:20 +0100

    multplikation funzt nicht so wirklich

--- Problemset 2/Mini_Project_1/main.adb
+++ Problemset 2/Mini_Project_1/main.adb
@@ -1,13 +1,18 @@
 with GF2n;
 
 procedure main is
-type Element_Type is mod 2**20;
-Irreducible_P : Element_Type := Element_Type'Last;
+type Element_Type is mod 2**9; --Irreducible needs 1 bit more
+Irreducible_P : Element_Type := 2#100011011#; --aus AES 100011011
 package GF2_4 is new GF2n(Element => element_type, F => Irreducible_P);
-x : element_type;
+x,y,z : element_type;
 
 begin
- x:= 1055;
+ x:= 16#53#;
+ y:= 16#ca#;
+ z:= 2;
  GF2_4.Put(x);
+ GF2_4.Put(x*y); -- solte 1 sein :-/
+ GF2_4.Put(y*z);
+ GF2_4.Put(x*z);
  GF2_4.Put(Irreducible_P);
 end main;
