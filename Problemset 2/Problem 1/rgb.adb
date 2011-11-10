30a937e7b7a25b23b725373fc5363af353778d93
Paul <paul@Turion-K685.(none)>
2011-11-10 13:23:45 +0100

    problem 4 marker init

--- Problemset 2/Problem 1/rgb.adb
+++ Problemset 2/Problem 1/rgb.adb
@@ -10,44 +10,88 @@ begin
 	Result(Blue)	:= BlueIntensity;
 	return Result;
 end To_Color;
-
+-- PLUS
 function "+"(Left : Color; Right : Color)  return Color is
 Result : Color;
 begin
-Result(Red) 	:= (Left(Red) + Right(Red)) mod (Intensity'Last+1);
-Result(Green)	:= (Left(Green) + Right(Green)) mod (Intensity'Last+1);
-Result(Blue) 	:= (Left(Blue) + Right(Blue)) mod (Intensity'Last+1);
+if Left(Red) + Right(Red) > Intensity'Last then
+	Result(Red) := Intensity'Last;
+else
+	Result(Red) := Left(Red) + Right(Red);
+end if;
+
+if Left(Green) + Right(Green) > Intensity'Last then
+	Result(Green) := Intensity'Last;
+else
+	Result(Green) := Left(Green) + Right(Green);
+end if;
+
+if Left(Blue) + Right(Blue) > Intensity'Last then
+	Result(Blue) := Intensity'Last;
+else
+	Result(Blue) := Left(Blue) + Right(Blue);
+end if;
+
 return Result;
 end "+";
 
-
+-- MINUS
 function "-"(Left : Color; Right : Color)  return Color is
 Result : Color;
 begin
-Result(Red) 	:= (Left(Red) - Right(Red)) mod (Intensity'Last+1);
-Result(Green)	:= (Left(Green) - Right(Green)) mod (Intensity'Last+1);
-Result(Blue) 	:= (Left(Blue) - Right(Blue)) mod (Intensity'Last+1);
+if Left(Red) - Right(Red) < Intensity'First then
+	Result(Red) := Intensity'First;
+else
+	Result(Red) := Left(Red) - Right(Red);
+end if;
+
+if Left(Green) - Right(Green) < Intensity'First then
+	Result(Green) := Intensity'First;
+else
+	Result(Green) := Left(Green) - Right(Green);
+end if;
+
+if Left(Blue) - Right(Blue) < Intensity'First then
+	Result(Blue) := Intensity'First;
+else
+	Result(Blue) := Left(Blue) - Right(Blue);
+end if;
 return Result;
 end "-";
 
-
+-- MULT
 function "*"(Left : Color; Right : Color)  return Color is
 Result : Color;
 begin
-Result(Red) 	:= (Left(Red) * Right(Red)) mod (Intensity'Last+1);
-Result(Green)	:= (Left(Green) * Right(Green)) mod (Intensity'Last+1);
-Result(Blue) 	:= (Left(Blue) * Right(Blue)) mod (Intensity'Last+1);
+if Left(Red) * Right(Red) > Intensity'Last then
+	Result(Red) := Intensity'Last;
+else
+	Result(Red) := Left(Red) * Right(Red);
+end if;
+
+if Left(Green) * Right(Green) > Intensity'Last then
+	Result(Green) := Intensity'Last;
+else
+	Result(Green) := Left(Green) * Right(Green);
+end if;
+
+if Left(Blue) * Right(Blue) > Intensity'Last then
+	Result(Blue) := Intensity'Last;
+else
+	Result(Blue) := Left(Blue) * Right(Blue);
+end if;
 return Result;
 end "*";
 
+-- EQUAL
 function "="(Left : Color; Right : Color)  return Boolean is
 begin
-return Left(Red) = Right(Red) and
-	Left(Green) = Right(Green) and
+return (Left(Red) = Right(Red) and
+	Left(Green) = Right(Green)) and
 	Left(Blue) = Right(Blue);
 
 end "=";
-
+-- PUT
 procedure Put(Item  : in Color) is
 begin
 Ada.Text_IO.Put ("R: ");
--- Problemset 3/MiniProject 4/mark_graph.adb
+++ Problemset 3/MiniProject 4/mark_graph.adb
@@ -1,4 +1,19 @@
 package body mark_graph is
 
+procedure Set_Mark(Graph  : in out Graph_Type; 
+                      Vertex : in     Vertex_Type;
+                      To     : in     Vertex_Mark) is
+
+begin
+
+end Set_Mark;
+
+function Get_Mark (Graph  : Graph_Type;
+                   Vertex : Vertex_Type) return Vertex_Mark is
+
+begin
+
+end Get_Mark;
+
 end mark_graph;
 
