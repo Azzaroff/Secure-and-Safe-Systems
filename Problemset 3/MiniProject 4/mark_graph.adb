0a7bc0e450f2ae46721e6d148a35df6c6350ff6f
Azzaroff <azzaroff@felux.fritz.box>
2011-11-10 14:18:48 +0100

    Marked Graph Implemented

--- Problemset 3/MiniProject 4/mark_graph.adb
+++ Problemset 3/MiniProject 4/mark_graph.adb
@@ -5,14 +5,13 @@ procedure Set_Mark(Graph  : in out Graph_Type;
                       To     : in     Vertex_Mark) is
 
 begin
-
+    Maps.Insert(Maps.Map(Graph.Map),Key => Vertex, New_Item => To);
 end Set_Mark;
 
 function Get_Mark (Graph  : Graph_Type;
                    Vertex : Vertex_Type) return Vertex_Mark is
-
 begin
-
+    return Maps.Element(Maps.Map(Graph.Map),Vertex);
 end Get_Mark;
 
 end mark_graph;
--- Problemset 3/MiniProject 4/mark_graph.ads
+++ Problemset 3/MiniProject 4/mark_graph.ads
@@ -1,8 +1,12 @@
 with Generic_Graph;
+with Ada.Containers.Indefinite_Hashed_Maps;
 
 generic
    type Vertex_Type is private;
    type Vertex_Mark is (<>);
+   with function "=" (X, Y: Vertex_Type ) return Boolean is <>;
+   with function Hash ( Item : Vertex_Type ) return Ada.Containers.Hash_Type is <>;
+
 package Mark_Graph is
  
    package Graphs is new Generic_Graph(Vertex_Type);
@@ -18,4 +22,17 @@ package Mark_Graph is
 
 private
     -- implementation dependent ..
+    package Maps is new Ada.Containers.Indefinite_Hashed_Maps
+        ( Key_Type => Vertex_Type,
+          Element_Type => Vertex_Mark,
+          Hash => Hash,
+          Equivalent_Keys => "=" );
+
+    type Mark_Map is new Maps.Map with null record; 
+
+    type Graph_Type is new Graphs.Graph_Type with record
+        Map : Mark_Map;
+    end record;
+
+
 end Mark_Graph;
