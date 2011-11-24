77e2d45b32a6e22793ce8e0a9a3b248a2632cef0
Paul <paul@Turion-K685.(none)>
2011-11-24 11:41:57 +0100

    CM hinzugefügt

---
+++ Problemset 4/Problem 2 Coffee Machine/coffee_machine.ads
@@ -0,0 +1,20 @@
+package Coffee_Machine is
+   -- Simulation of a coin-driven coffee machine
+   -- User: - One slot to insert coins (only, 10 or, 20 cents)
+   --       - One button to press (''money back'')
+   -- Machine: one slot to drop coins, the coffee output
+   -- Given 30 cents or more, the coffee is produced immediately
+   -- (Note that Overspending is Possible)
+      
+   type State is private;
+   type Action is(Ten_Cent, Twenty_Cent, Button);
+   type Reaction is(Nothing, Drop_All_Coins, Coffee);
+   
+   procedure Initialize( X : out State);
+   procedure X(S     : in out State;
+	       Act   : in Action;
+	       React : out Reaction);
+   
+private
+   type State is range 0..2;  
+end Coffee_Machine;
