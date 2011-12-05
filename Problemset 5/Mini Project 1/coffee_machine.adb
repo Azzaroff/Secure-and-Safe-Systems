787d47d0a7ced78fa8242f7eadb3a2c9fb0d640f
Paul <paul@TurionK685.(none)>
2011-12-05 08:59:47 +0100

    PS 5 initial

---
+++ Problemset 5/Mini Project 1/coffee_machine.adb
@@ -0,0 +1,48 @@
+package body coffee_machine is
+
+procedure Initialize( X : out State) is
+begin
+X := 0; -- init an empty credit
+end Initialize;
+
+-- there are three states:
+-- 0: wait for money
+-- 1: ten cent where given to the machine
+-- 2: twenty cent whre given to the machine
+procedure X(S     : in out State;
+	       Act   : in Action;
+	       React : out Reaction) is
+begin
+
+case Act  is
+	when Ten_Cent =>
+		if S = 0 then -- add the money to the credit
+			S	:= 1;
+			React	:= Nothing;
+		elsif S = 1 then -- add the money to the credit
+			S	:= 2;
+			React	:= Nothing;
+		else -- credit is enought to produce coffee
+			S	:= 0;
+			React	:= Coffee;
+		end if;		
+	when Twenty_Cent =>
+		if S = 0 then -- add the money to the credit
+			S	:= 2;
+			React	:= Nothing;
+		else -- es ist egal ob schon 10 oder 20ct drin waren, Kaffee gibts sowieso und der Rest vom Geld ist weg
+			S	:= 0;
+			React	:= Coffee;
+		end if;
+	when Button =>
+		if S = 0 then -- no money was given
+			S 	:= 0; -- evtl. weglassen
+			React	:= Nothing;
+		else -- some money was given to the machine
+			S 	:= 0;
+			React 	:= Drop_all_Coins;
+		end if;
+end case;
+end X;
+
+end coffee_machine;
---
+++ Problemset 5/Mini Project 1/coffee_machine.ads
@@ -0,0 +1,59 @@
+package Coffee_Machine is
+   -- Simulation of a coin-driven coffee machine
+   -- User: - One slot to insert coins (only, 10 or, 20 cents)
+   --       - One button to press (''money back'')
+   -- Machine: one slot to drop coins, the coffee output
+   -- Given 30 cents or more, the coffee is produced immediately
+   -- (Note that Overspending is Possible)
+      
+   type State is range 0..2;  
+   type Action is(Ten_Cent, Twenty_Cent, Button);
+   type Reaction is(Nothing, Drop_All_Coins, Coffee);
+
+   -- pre conditions not necessary, because very possible input can occur
+   -- no need for range checks because compiler checks that
+
+   procedure Initialize( X : out State) with
+   post => (X = 0); 
+
+   procedure X(S     : in out State;
+	       Act   : in Action;
+	       React : out Reaction) with
+   post => (if (S'old /= S) then (
+             if Act = Ten_Cent then (
+                 if S'Old = 0 then (
+			React = Nothing and
+			S = 1 
+		)elsif S'Old = 1 then (
+			React = Nothing and
+			S = 2
+		)elsif S'Old = 2 then (
+			React = Coffee and
+			S = 0
+		)
+             )elsif Act = Twenty_Cent then (
+		if S'Old = 0 then (
+			React = Nothing and
+			S = 2
+		)elsif S'Old = 1 then (
+			React = Coffee and
+			S = 0
+		)elsif S'Old = 2 then (
+			React = Coffee and
+			S = 0
+		)
+	     )elsif Act = Button then (
+		if S'Old = 0 then (
+			React = Nothing and
+			S = 0
+		)elsif S'Old = 1 then (
+			React = Drop_All_Coins and
+			S = 0
+		)elsif S'Old = 2 then (
+			React = Drop_All_Coins and 
+			S = 0
+		)
+		)
+           )); 
+   
+end Coffee_Machine;
