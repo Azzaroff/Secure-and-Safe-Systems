3d69111e2e47d35f7c3260d166f16605a21399a4
Paul <paul@TurionK685.(none)>
2012-01-17 20:55:50 +0100

    Hofstaeter ohne Time abort

--- Problemset 7/Mini Project 1/hofstadter_parallel.adb
+++ Problemset 7/Mini Project 1/hofstadter_parallel.adb
@@ -3,9 +3,13 @@ with Armageddon;
 
 package body Hofstadter_Parallel is
 
+Ragnaroek : Abort_Tasks;
 
 function Q(N: Positive) return Positive is
    begin
+	  if Ragnaroek.Is_Done then
+		  return 1;
+	  end if;
       if N < 3 then
          return 1;
       else
@@ -33,7 +37,9 @@ protected body Hofstadter_Array is
 	begin
 	   Ada.Text_IO.Put_Line("print:");
 	   for I in List'range loop
-		Ada.Text_IO.Put_Line(I'Img & List(I)'Img);
+		if List(I) > 0 then
+			Ada.Text_IO.Put_Line(I'Img & List(I)'Img);
+		end if;
    	   end loop;
 	end Print;
 end Hofstadter_Array;
@@ -66,7 +72,7 @@ end Abort_Tasks;
 procedure Mute_Workers (End_Value : Integer; End_Time : Duration) is
 	hof_array : Hofstadter_Array(End_Value);
 	one_counter : One_Way_Counter(4);
-    Ragnaroek : Abort_Tasks;
+
 	
 
 	task type Worker is
@@ -93,7 +99,6 @@ procedure Mute_Workers (End_Value : Integer; End_Time : Duration) is
 			Cnt	:= Counter;
 			E_T     := End_Time;
 		end Start;
-		Ada.Text_IO.Put_Line("Counter: " & Cnt'Img);
 		while Val <= Cnt loop
 			Ada.Text_IO.Get_Immediate (Command, Available);
 			if (Available and Command = 'q') or Ragnaroek.Is_Done then
@@ -104,7 +109,9 @@ procedure Mute_Workers (End_Value : Integer; End_Time : Duration) is
 			hof_array.Get_Value(hof_val, hof_eval);
 			if not hof_eval then
 				hof_val := Q(Val);
-				hof_array.Put_Value(Val, hof_val);
+				if not Ragnaroek.Is_Done then
+					hof_array.Put_Value(Val, hof_val);
+				end if;
 			end if;
 			Ada.Text_IO.Put_Line(My_Name & " " & Val'Img & hof_val'Img);
 			Val := Val + Diff;
@@ -117,14 +124,20 @@ begin
     declare
         O, Tw, Th, F: Worker;--, Fi, S, Se, E : Worker;
     begin --declare
-       O.Start('1', 1, 4, End_Value, End_Time);
-       Tw.Start('2', 2, 4, End_Value, End_Time);
-       Th.Start('3', 3, 4, End_Value, End_Time);
-       F.Start('4', 4, 4, End_Value, End_Time);
+		select
+			Delay End_Time;
+			Ada.Text_IO.Put_Line("select");
+		then abort
+			--Ragnaroek.Is_Done;
+	   		O.Start('1', 1, 4, End_Value, End_Time);
+       		Tw.Start('2', 2, 4, End_Value, End_Time);
+       		Th.Start('3', 3, 4, End_Value, End_Time);
+       		F.Start('4', 4, 4, End_Value, End_Time);
 --       Fi.Start('5', 5, 8, End_Value, End_Time);
 --       S.Start('6', 6, 8, End_Value, End_Time);
 --       Se.Start('7', 7, 8, End_Value, End_Time);
 --       E.Start('8', 8, 8, End_Value, End_Time);
+		end select;
     end; --declare
 
 
