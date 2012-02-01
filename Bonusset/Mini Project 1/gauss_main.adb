522a4097c3e03ac956cbb17a4448cd6ba6374e8a
Azzaroff <azzaroff@felux.fritz.box>
2012-02-01 16:36:57 +0100

    proofed mit dem fucking checker

--- Bonusset/Mini Project 1/gauss_main.adb
+++ Bonusset/Mini Project 1/gauss_main.adb
@@ -2,9 +2,9 @@ with Ada.Text_IO, Gauss;
 with Ada.Command_Line; use Ada.Command_Line;
 
 procedure gauss_main is
-
-
+res : Positive;
 begin
 	Ada.Text_IO.Put("Gauss sum of " & Argument(1) & " =");
-    Ada.Text_IO.Put_Line(Gauss.gauss_sum(Gauss.Gauss_Positive'Value(Argument(1)))'Img);
+    Gauss.gauss_sum(Gauss.Gauss_Positive'Value(Argument(1)),res);
+    Ada.Text_IO.Put_Line(res'Img);
 end gauss_main;
