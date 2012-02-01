6ee8bf0b3245e573d8f9b5af7c2cb971f7bd5712
Paul <paul@TurionK685.(none)>
2012-02-01 19:11:43 +0100

    MP2 geht immer noch nicht

--- Bonusset/Mini Project 2/faculty.adb
+++ Bonusset/Mini Project 2/faculty.adb
@@ -1,19 +1,17 @@
 package body Faculty is
 	-- maximum n = 12 fac := 12! = 479001600
-	function calculate_faculty(n : in Faculty_Positive) return Positive is
-	fac : Positive := 1;
-	i 	: Integer := 0;
+	procedure calculate_faculty(n : in Faculty_Positive; fac : out Positive) is
+	i 	: Faculty_Positive := 1;
 	begin
-		--# Assert i >= 0 and fac = 1;
+		fac := 1;
+		--# Assert i >= 1 and i <= n and n <= Faculty_Positive'Last and n >= Faculty_Positive'First and fac*i <= Positive'Last and fac*i >= Positive'First 
+		--# and fac >= 1 and fac = fac_arr(i);
 		while i < n loop
-			--# Assert i >= 0 and i < n and n>= Faculty_Positive'First and n <= Faculty_Positive'Last and (fac*(i+1)) <= Positive'Last;
-			i := i + 1;	
-			-- Assert i <= n and i <= Integer'Last and fac = fac_arr(i-1) and fac*i <= Positive'Last ;			
-			fac := fac * i;				
-			-- Assert i <= n and fac = fac_arr(i) and fac <= Positive'Last and fac >= Positive'First;
+			--# Assert i < n and n <= Faculty_Positive'Last and n >= Faculty_Positive'First and fac*i <= Positive'Last and fac*i >= Positive'First and fac = fac_arr(i);
+			fac := fac * i;
+			i := i + 1;				
 		end loop;
-		-- Assert i >= n and fac = fac_arr(n) and fac <= Positive'Last and fac >= Positive'First;
-		return fac;
+		--# Assert fac = fac_arr(i);
 	end calculate_faculty;
 
 end Faculty;
--- Bonusset/Mini Project 2/faculty_main.adb
+++ Bonusset/Mini Project 2/faculty_main.adb
@@ -3,7 +3,9 @@ with Ada.Command_Line; use Ada.Command_Line;
 
 
 procedure Faculty_Main is
+result : Positive;
 begin
 	Ada.Text_IO.Put("The faculty of n ="&Argument(1)&" is ");
-	Ada.Text_IO.Put_Line(Faculty.calculate_faculty(Integer'Value(Argument(1)))'Img);
+	Faculty.calculate_faculty(Integer'Value(Argument(1)), result);
+	Ada.Text_IO.Put_Line(result'Img);
 end Faculty_Main;
